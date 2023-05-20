4925d9c88b25db00ea9b124438fb55d9afca1325
Aaqil yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2023-05-20 20:02:24 +0530

    Create fruits.js

---
+++ fruits.js
@@ -0,0 +1,6 @@
+// Step 1: Create the "fruits" array
+var fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+// Step 2: Display the total number of fruits
+var totalFruits = fruits.length;
+console.log("Total number of fruits: " + totalFruits);
